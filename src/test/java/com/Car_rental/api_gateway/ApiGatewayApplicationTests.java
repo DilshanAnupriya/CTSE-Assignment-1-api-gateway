11c9ad4e4e094fa87814078c846723d80f382a67
Dilshan <dilshananupriya@icloud.com>
2026-03-22 11:59:26 +0530

    gateway setup

---
+++ src/test/java/com/Car_rental/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Car_rental.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
